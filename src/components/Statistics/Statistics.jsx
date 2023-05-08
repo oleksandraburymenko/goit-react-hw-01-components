345c7544fd60c5ed8cf3e83895fd8b72be90e258
oleksandraburymenko <115561772+oleksandraburymenko@users.noreply.github.com>
2023-05-08 21:01:12 +0300

    add Statistics jsx

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,40 @@
+import css from './Statistics.module.css'
+import PropTypes from 'prop-types';
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+export const Statistics = ({title, stats}) => {
+    return (
+        <section className={css.statistics}>
+            {title && <h2 className={css.title}>{title}</h2>}
+            <ul className={css.statList}>
+                {stats.map(({ id, label, percentage }) => (
+                    <li
+                    key={id}
+                    className={css.item}
+                    style={{ backgroundColor: getRandomColor () }}
+                    >
+                        <span className={css.label}>{ label }</span>
+                        <span className={css.percentage}>{ percentage }%</span>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    )
+}
+
+
+Statistics.PropTypes = {
+    title: PropTypes.string.isRequired,
+    stats: PropTypes.array.isRequired,
+    id: PropTypes.string,
+    label: PropTypes.string,
+    persentage: PropTypes.number,
+}
